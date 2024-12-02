973d282e467d8ec25ed7d5bb115e699b24e4a677
PastRecall <dy_lamba@bk.ru>
2024-12-02 16:52:54 +0300

    Исправление опечатки и добавление эксектов в тест из PO

--- tests/search.test.js
+++ tests/search.test.js
@@ -1,13 +1,14 @@
 import { test } from '@playwright/test';
 import { allure } from "allure-playwright";
 import { App } from '../origin/pages/app.page';
+import { expect } from '@playwright/test';
 
 const url = 'https://www.tretyakovgallery.ru/';
 let app;
 let searchValue;
 
 test('Повторное использование поиска', async ({ page }) => {
-    await allure.tags ("searsh");
+    await allure.tags ("search");
     await allure.owner("Akhatova");
     app = new App(page);
     searchValue = 'Пушкин';
@@ -15,12 +16,14 @@ test('Повторное использование поиска', async ({ page
     await app.mainPage.open(url); 
     await app.mainPage.search();
     await app.searchPage.fillsearchField(searchValue);
-    await app.resultPage.resultSearch(searchValue);
+    await allure.step(`Заголовок статьи содержит значение - "${searchValue}"`, async () => {
+    await expect(app.resultPage.searchResult.nth(0)).toHaveText(new RegExp(`.*${searchValue}.*`));
+    });
     await app.resultPage.resultClear();
-    
     searchValue = 'Италия';
     await app.searchPage.fillsearchField(searchValue);
-    await app.resultPage.resultSearch(searchValue);
- 
+    await allure.step(`Заголовок статьи содержит значение - "${searchValue}"`, async () => {
+    await expect(app.resultPage.searchResult.nth(0)).toHaveText(new RegExp(`.*${searchValue}.*`));
+    });
 });
 
